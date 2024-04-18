f28a4fb0dcad0b4ef179d918cc613eefdadab1d3
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-04-18 14:45:56 -0300

    feat: validate check-in use case and tests

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -18,6 +18,14 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkIn
   }
 
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id)
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn
+    }
+    return checkIn
+  }
+
   async findByUserIdOnDate(
     userId: string,
     date: Date,
@@ -37,6 +45,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     }
 
     return checkInOnSameDate
+  }
+
+  async findById(checkInId: string): Promise<CheckIn | null> {
+    const checkIn = this.items.find((checkIn) => checkIn.id === checkInId)
+
+    if (!checkIn) {
+      return null
+    }
+
+    return checkIn
   }
 
   async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
---
+++ src/use-cases/validate-check-in.ts
@@ -0,0 +1,47 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+
+import dayjs from 'dayjs'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface ValidateCheckInUseCaseRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInUseCase {
+  private MAX_CHECKIN_VALIDATION_IN_MINUTES = 20
+
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (
+      distanceInMinutesFromCheckInCreation >
+      this.MAX_CHECKIN_VALIDATION_IN_MINUTES
+    ) {
+      throw new LateCheckInValidationError()
+    }
+
+    checkIn.validated_at = new Date()
+    this.checkInsRepository.save(checkIn)
+
+    return { checkIn }
+  }
+}
