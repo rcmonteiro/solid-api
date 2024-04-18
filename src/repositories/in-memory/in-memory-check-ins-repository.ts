4c532824694a91680cbd8890b37855af43c6442b
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-04-18 12:46:06 -0300

    feat: check in history use case and test

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -38,4 +38,14 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkInOnSameDate
   }
+
+  async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    const checkIns = this.items
+      .filter((checkIn) => {
+        return checkIn.user_id === userId
+      })
+      .slice((page - 1) * 20, page * 20)
+
+    return checkIns
+  }
 }
